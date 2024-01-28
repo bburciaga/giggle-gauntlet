22d928563223d4e1892aebc3fb091e98f03476b2
A-dith-ya <awedithya@gmail.com>
2024-01-27 22:13:13 -0800

    Add image health display

--- scenes/gauntlet/Gauntlet.gd
+++ scenes/gauntlet/Gauntlet.gd
@@ -5,3 +5,18 @@ func _draw():
 	draw_rect(Rect2(0.0, 650.0, 1167.0, 16.0), Color.BLACK)
 	draw_rect(Rect2(0.0, 0.0, 16.0, 650.0), Color.BLACK)
 	draw_rect(Rect2(1156.0, 0.0, 16.0, 650.0), Color.BLACK)
+
+func _process(delta):
+	match $Player.health:
+		1:
+			$UI/Health.play("1")
+		2:
+			$UI/Health.play("2")
+		3:
+			$UI/Health.play("3")
+		4:
+			$UI/Health.play("4")
+		5:
+			$UI/Health.play("5")
+		6:
+			$UI/Health.play("6")
