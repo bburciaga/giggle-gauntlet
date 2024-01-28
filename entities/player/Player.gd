001129f46e3201028b57e62d5bcc70ab1f1a353c
A-dith-ya <awedithya@gmail.com>
2024-01-27 18:26:22 -0800

    Reduce gnome weapon hitbox

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -8,9 +8,12 @@ enum WeaponState {
 @onready var anim = get_node("AnimatedSprite2D")
 
 var SPEED = 300
+var INITIAL_WEAPON_POSITION = Vector2(915, -5)
+var enemy
+var weaponState = WeaponState.INACTIVE
 
 func _ready():
-	gnomeWeapon.position = Vector2(915, -5)
+	gnomeWeapon.position = INITIAL_WEAPON_POSITION
 	
 func _physics_process(delta):
 	var direction = Input.get_vector(
@@ -24,18 +27,22 @@ func _physics_process(delta):
 	if direction.length() > 0:
 		anim.play("Run")
 		anim.flip_h = direction.x < 0
-		
 	else:
 		anim.play("Idle")
-		
 	move_and_slide()
 	
 	if Input.is_action_pressed("ui_accept"):
 		if (weaponState == WeaponState.GNOME):
-			gnomeWeapon.position = Vector2(15, -5)
+			if(direction.x < 0):
+				gnomeWeapon.rotation_degrees = -45.0
+				gnomeWeapon.position = Vector2(-15, -5)
+			else:
+				gnomeWeapon.rotation_degrees = 45.0
+				gnomeWeapon.position = Vector2(15, -5)
 			gnomeWeapon.visible = true
 	else:
-		gnomeWeapon.visible = false
+		gnomeWeapon.position = INITIAL_WEAPON_POSITION
+		gnomeWeapon.visible = true
 
 func _on_gnome_hit_area_entered(area):
 	enemy.take_damage()
