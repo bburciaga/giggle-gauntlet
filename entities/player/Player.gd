387860dcc68055a82ca1f38ec18372dedc06618d
A-dith-ya <awedithya@gmail.com>
2024-01-27 17:51:29 -0800

    Implement weapon movement into Player hand

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -1,9 +1,17 @@
 extends CharacterBody2D
 
+enum WeaponState {
+	INACTIVE,
+	GNOME
+}
+
 @onready var anim = get_node("AnimatedSprite2D")
 
 var SPEED = 300
 
+func _ready():
+	gnomeWeapon.position = Vector2(915, -5)
+	
 func _physics_process(delta):
 	var direction = Input.get_vector(
 		"move_left",
@@ -16,7 +24,22 @@ func _physics_process(delta):
 	if direction.length() > 0:
 		anim.play("Run")
 		anim.flip_h = direction.x < 0
+		
 	else:
 		anim.play("Idle")
 		
 	move_and_slide()
+	
+	if Input.is_action_pressed("ui_accept"):
+		if (weaponState == WeaponState.GNOME):
+			gnomeWeapon.position = Vector2(15, -5)
+			gnomeWeapon.visible = true
+	else:
+		gnomeWeapon.visible = false
+
+func _on_gnome_hit_area_entered(area):
+	enemy.take_damage()
+	
+func activate_gnome_weapon():
+	weaponState = WeaponState.GNOME
+	gnomeWeapon.visible = true
