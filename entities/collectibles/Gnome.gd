e0e7a38ef7349167445b64b0d74e4f56db7fbb49
A-dith-ya <awedithya@gmail.com>
2024-01-27 20:06:57 -0800

    Add gnome collectible rotation

--- entities/collectibles/Gnome.gd
+++ entities/collectibles/Gnome.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@onready var gnome = get_node("AnimatedSprite2D")
+
+func _ready():
+	gnome.play("rotate")
+	
 func _on_body_entered(body):
 	if body.name == "Player":
 		body.activate_gnome_weapon()
