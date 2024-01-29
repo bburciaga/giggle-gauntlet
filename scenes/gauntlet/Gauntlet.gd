132c43ca579943d56fe66c6e365e463a60dcbfd4
A-dith-ya <awedithya@gmail.com>
2024-01-28 21:20:45 -0800

    Add User Weapon UI

--- scenes/gauntlet/Gauntlet.gd
+++ scenes/gauntlet/Gauntlet.gd
@@ -1,4 +1,14 @@
 extends Node2D
+class_name Globals
+
+enum WeaponState {
+	INACTIVE,
+  	WATERGUN,
+	GNOME,
+	BANANA
+}
+
+
 
 func _draw():
 	draw_rect(Rect2(0.0, 0.0, 1167.0, 16.0), Color.BLACK)
@@ -6,7 +16,22 @@ func _draw():
 	draw_rect(Rect2(0.0, 0.0, 16.0, 650.0), Color.BLACK)
 	draw_rect(Rect2(1156.0, 0.0, 16.0, 650.0), Color.BLACK)
 
+var adjusted_width = 100.0
+var adjusted_height = 100.0
+
 func _process(delta):
+	$UI/Gnome.visible = false
+	$UI/Watergun.visible = false
+	$UI/Banana.visible = false
+	match $Player.weaponState:
+		Globals.WeaponState.GNOME:
+			$UI/Gnome.visible = true
+		Globals.WeaponState.WATERGUN:
+			$UI/Watergun.visible = true
+		Globals.WeaponState.BANANA:
+			$UI/Banana.visible = true
+			$UI/Banana.play("Default")
+		
 	match $Player.health:
 		1:
 			$UI/Health.play("1")
