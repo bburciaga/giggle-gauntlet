7ee6885b5252e450eeb901323c730793c09f5129
A-dith-ya <awedithya@gmail.com>
2024-01-29 12:06:04 -0800

    Add player score

--- menus/finish/Finish.gd
+++ menus/finish/Finish.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+@onready var player_vars = get_node("/root/PlayerVariables")
+
+func _ready():
+	$Label.text = "Score " + str(player_vars.score)
 
 func _on_play_again_pressed():
 	get_tree().change_scene_to_file("res://scenes/gauntlet/Gauntlet.tscn")
-
 
 func _on_quit_pressed():
 	get_tree().quit()
